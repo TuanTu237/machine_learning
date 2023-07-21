e2e8b44d2b1dcf30c3dec95637e66dfd0f37c38b
TuanTu237 <tuvieclam247@gmail.com>
2023-07-21 14:46:51 +0700

    update

--- onemonth.py
+++ onemonth.py
@@ -1,11 +1,32 @@
-class Person:
-  def __init__(self, name , age):
-    self.name = name
-    self.age = age
-  def __str__(self):
-    return f"{self.name}{self.age}"
-  def myfunc(seft):
-    print("hello " + seft.name)
-p1 = Person("tu", 21)
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
 
-p1.myfunc()
+  def move(self):
+    print("Drive!")
+
+class Boat:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Sail!")
+
+class Plane:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang")       #Create a Car class
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
+plane1 = Plane("Boeing", "747")     #Create a Plane class
+
+for x in (car1, boat1, plane1):
+  x.move()
+
+print(car1)
