77d87fba07b4a40446d71e278c1032e55f1981c4
TuanTu237 <tuvieclam247@gmail.com>
2023-07-21 22:28:57 +0700

    up

--- onemonth.py
+++ onemonth.py
@@ -1,8 +1,10 @@
-import re
-
-txt = "The rain in Spain"
-
-x = re.search("\s", txt)
 
 
-print(x)
+
+f = open("demo.txt", "a")
+f.write("Woops! I have deleted the content!")
+f.close()
+
+#open and read the file after the overwriting:
+f = open("demo.txt", "r")
+print(f.read())
