4ce6703c5aff4483705189f04f32a8d57873166a
TuanTu237 <tuvieclam247@gmail.com>
2023-07-21 10:19:18 +0700

    update

--- onemonth.py
+++ onemonth.py
@@ -1,6 +1,16 @@
-thisset = {"apple", "banana", "cherry"}
+thisdict ={
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 2002,
+    "color": ["red", "while","blue"]
+}
+print(len(thisdict))
+print(type(thisdict))
 
-x = thisset.pop()
-
+mydict = dict(name = "tu" , age = 21 ,country = "vn")
+print(mydict)
+x = mydict["name"]
+y = mydict.get("age")
+z = mydict.keys()
 print(x)
-print(thisset)
+print(y,z)
