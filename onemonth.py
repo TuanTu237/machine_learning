c531db306aef9a4cde403362d7f81f50bb2a2907
TuanTu237 <tuvieclam247@gmail.com>
2023-07-22 22:07:19 +0700

    up nhu liet ke cac thu tu cua cac so

--- onemonth.py
+++ onemonth.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
 
-newarr = arr.reshape(2, 3, 2)
-
-print(newarr)
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
