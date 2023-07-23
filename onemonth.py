28db292266bcfbfc2d30c25e97b0ff26c2de025c
TuanTu237 <tuvieclam247@gmail.com>
2023-07-23 09:08:02 +0700

    up

--- onemonth.py
+++ onemonth.py
@@ -1,9 +1,15 @@
 import numpy as np
 
-arr = np.array([41, 42, 43, 44])
+arr = np.array([1,2,3,4,5,6])
 
-x = [True, False, True, False]
+filter_arr = []
 
-newarr = arr[x]
+for element in arr:
+    if element %2 ==0:
+        filter_arr.append(True)
+    else:
+        filter_arr.append(False)
 
+newarr = arr[filter_arr]
+print(filter_arr)
 print(newarr)
