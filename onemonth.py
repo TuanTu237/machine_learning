4dcbf99c3cda0c7e0c38e5f748215d7ab25d45e1
TuanTu237 <tuvieclam247@gmail.com>
2023-07-26 22:57:00 +0700

    up

--- onemonth.py
+++ onemonth.py
@@ -1,3 +1,12 @@
-from scipy import constants
+from scipy.optimize import root
+from math import cos
 
-print(constants.pi)
+
+def eqn(x):
+    return x + cos(x)
+
+
+myroot = root(eqn, 0)
+
+print(myroot.x)
+print(myroot)
