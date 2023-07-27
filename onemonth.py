f24ff1875581e1bdfe35e7a499037f75d70c0e7b
TuanTu237 <tuvieclam247@gmail.com>
2023-07-27 21:38:25 +0700

    up

--- onemonth.py
+++ onemonth.py
@@ -1,20 +1,8 @@
 import numpy as np
-from scipy.spatial import ConvexHull
-import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis
 
-points = np.array([
-    [2, 3],
-    [2, 4],
-    [2, 24],
-    [5, 1]
-])
+v = np.random.normal(size=100)
 
-hull = ConvexHull(points)
-hull_points = hull.simplices
+print(skew(v))
 
-plt.scatter(points[:, 0], points[:, 1])
-
-for simplex in hull_points:
-    plt.plot(points[simplex, 0], points[simplex, 1], 'k-')
-
-plt.show()
+print(kurtosis(v))
