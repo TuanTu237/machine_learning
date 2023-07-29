b8d96d423e46ea34eb57f07501254e49ecebcc6c
TuanTu237 <tuvieclam247@gmail.com>
2023-07-29 12:50:47 +0700

    up

--- onemonth.py
+++ onemonth.py
@@ -1,14 +1,12 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import metrics
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
 
-actual = np.random.binomial(1, .9, size=1000)
-predicted = np.random.binomial(1, .0, size=1000)
+iris = datasets.load_iris()
 
-confusion_matrix = metrics.confusion_matrix(actual, predicted)
+X = iris['data']
+y = iris['target']
 
-cm_display = metrics.ConfusionMatrixDisplay(
-    confusion_matrix=confusion_matrix, display_labels=[False, True])
+logit = LogisticRegression(max_iter=10000)
 
-cm_display.plot()
-plt.show()
+print(logit.fit(X, y))
+print(logit.score(X, y))
