4d26c92695db9faa7b42f6bac780c34ebc7672ea
TuanTu237 <tuvieclam247@gmail.com>
2023-07-30 16:14:54 +0700

    up

--- onemonth.py
+++ onemonth.py
@@ -1,17 +1,31 @@
-from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import BaggingClassifier
 
-x = [4, 5, 10, 4, 3, 11, 14, 6, 10, 12]
-y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
-data = list(zip(x, y))
-inertias = []
+data = datasets.load_wine(as_frame=True)
 
-for i in range(1, 11):
-    kmeans = KMeans(n_clusters=i)
-    kmeans.fit(data)
-    inertias.append(kmeans.inertia_)
+X = data.data
+y = data.target
 
-plt.plot(range(1, 11), inertias, marker='o')
-plt.title('Elbrow method')
-plt.xlabel('number of clusters')
-plt.ylabel('inertia')
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=22)
+
+estimator_range = [2, 4, 6, 8, 10, 12, 14, 16]
+
+models = []
+scores = []
+
+for n_estimators in estimator_range:
+    clf = BaggingClassifier(n_estimators=n_estimators, random_state=22)
+    clf.fit(X_train, y_train)
+    models.append(clf)
+    scores.append(accuracy_score(y_true=y_test, y_pred=clf.predict(X_test)))
+
+
+plt.igure(figsize=(9, 6))
+
+plt.plot(estimator_range, score)
+
 plt.show()
